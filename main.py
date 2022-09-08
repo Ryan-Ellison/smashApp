01b150ca33d2dbc0018a4605eb4bfbd1f032cf0e
Ryan-Ellison <70408855+Ryan-Ellison@users.noreply.github.com>
2022-09-08 12:02:13 -0400

    Learning Kivy

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+# import module
 import numpy as np
-import pandas as pd
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+kivy.require('2.1.0')
 
 
-# Press the green button in the gutter to run the script.
+class CharacterScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(CharacterScreen, self).__init__(**kwargs)
+        self.cols = 5
+        for x in charactersList:
+            self.add_widget(Label(text=x))
+
+class MyApp(App):
+    def build(self):
+        return CharacterScreen()
+
+
+if __name__ == '__main__':
+    charactersList = np.array(open("Characters.txt", "r").read().splitlines())
+    MyApp().run()
